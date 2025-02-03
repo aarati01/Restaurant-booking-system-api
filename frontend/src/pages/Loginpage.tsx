d3814aac5bb0da2171e882d25329303c3225c4b6
aarati01 <131001442+aarati01@users.noreply.github.com>
2025-02-02 23:48:18 -0500

    anchor tag is added to all page

--- frontend/src/pages/Loginpage.tsx
+++ frontend/src/pages/Loginpage.tsx
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import Navigation from "./Navigation.tsx";
+
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -60,59 +62,63 @@ export const LoginPage: React.FC = () => {
   };
 
   return (
-    <div
-      className="flex items-center justify-center min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('/image/background.jpg')" }}
-    >
-      <div className="bg-olive p-6 md:p-8 rounded-lg shadow-lg w-full max-w-md m-auto  ">
-        <img
-          src="loginLogo.jpg"
-          alt="logo of the login"
-          className="w-24 h-24 m-auto block bg-black-600 border "
-        ></img>
-        <h1 className="text-center text-2xl font-semibold text-gray-800 mb-4">
-          Login
-        </h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <div>
-            <input
-              {...register("username")}
-              placeholder="Username"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <p className="text-red-500 text-sm mt-1">
-              {errors.username?.message}
-            </p>
-          </div>
+    <>
+      <div
+        className="flex items-center justify-center min-h-screen bg-cover bg-center"
+        style={{ backgroundImage: "url('/image/background.jpg')" }}
+      >
+        <Navigation />
 
-          <div>
-            <input
-              {...register("password")}
-              type="password"
-              placeholder="Password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <p className="text-red-500 text-sm mt-1">
-              {errors.password?.message}
-            </p>
-          </div>
-
-          <button
-            type="submit"
-            className="w-full py-2 bg-background font-rufinab text-white rounded-md hover:bg-lime-800 transition duration-300"
-          >
+        <div className="bg-olive p-6 md:p-8 rounded-lg shadow-lg w-full max-w-md m-auto  ">
+          <img
+            src="loginLogo.jpg"
+            alt="logo of the login"
+            className="w-24 h-24 m-auto block bg-black-600 border "
+          ></img>
+          <h1 className="text-center text-2xl font-semibold text-gray-800 mb-4">
             Login
-          </button>
+          </h1>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <input
+                {...register("username")}
+                placeholder="Username"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <p className="text-red-500 text-sm mt-1">
+                {errors.username?.message}
+              </p>
+            </div>
 
-          <p className="text-center text-gray-600 mt-2">
-            Don't have an account?{" "}
-            <Link to="/signup" className="text-blue-500 hover:underline">
-              Sign up here.
-            </Link>
-          </p>
-        </form>
+            <div>
+              <input
+                {...register("password")}
+                type="password"
+                placeholder="Password"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <p className="text-red-500 text-sm mt-1">
+                {errors.password?.message}
+              </p>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full py-2 bg-background font-rufinab text-white rounded-md hover:bg-lime-800 transition duration-300"
+            >
+              Login
+            </button>
+
+            <p className="text-center text-gray-600 mt-2">
+              Don't have an account?{" "}
+              <Link to="/signup" className="text-blue-500 hover:underline">
+                Sign up here.
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/pages/Navigation.tsx
+++ frontend/src/pages/Navigation.tsx
@@ -1,34 +1,45 @@
-import '../pages/mycss.css';
+import { Link } from "react-router-dom";
+import "../pages/mycss.css";
 
 const Navigation = () => {
   return (
     <div className="landingpagediv">
       <div className="insideLanding">
         <nav className="nav">
-        <img 
-        width={40}
-        height={40}
-            src="/logo.jpg" 
-            alt="Hamro Restaurant Logo" 
-            className="w-20 h-20 ml-3 " 
-          />          {/* <h1 className="resturantName ml-3 text-cyan-800 hover:text-cyan-600 text-2xl font-serif font-bold">
-            Hamro Resturant
-          </h1> */}
+          <img
+            width={40}
+            height={40}
+            src="/logo.jpg"
+            alt="Hamro Restaurant Logo"
+            className="w-20 h-20 ml-3"
+          />
           <ul className="flex space-x-4 m-3">
             <li className="li relative group">
-              <span className="flex items-center cursor-pointer">
-                Menue</span>
-        
+              <span className="flex items-center cursor-pointer">Menue</span>
               <ul className="absolute hidden group-hover:block bg-white shadow-md rounded mt-1">
-                <li className="li px-4 py-2 hover:bg-gray-200 cursor-pointer">Lunch</li>
-                <li className="li px-4 py-2 hover:bg-gray-200 cursor-pointer">Breakfast</li>
-                <li className="li px-4 py-2 hover:bg-gray-200 cursor-pointer">Dinner</li>
+                <li className="li px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                  <Link to="/menu?type=lunch">Lunch</Link>
+                </li>
+                <li className="li px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                  <Link to="/menu?type=breakfast">Breakfast</Link>
+                </li>
+                <li className="li px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                  <Link to="/menu?type=dinner">Dinner</Link>
+                </li>
               </ul>
             </li>
-            <li className="li">Login</li>
-            <li className="li">Online Order</li>
-            <li className="li">Reservation</li>
-            <li className="li">Contact</li>
+            <li className="li">
+              <Link to="/login">Login</Link>
+            </li>
+            <li className="li">
+              <Link to="/menu">Online Order</Link>
+            </li>
+            <li className="li">
+              <Link to="/reservation">Reservation</Link>
+            </li>
+            <li className="li">
+              <Link to="/contact">Contact</Link>
+            </li>
           </ul>
         </nav>
       </div>
